b7cbd3b9c974128d21541d7309db812113fc2941
Neeyo1 <jakub.nycz99@gmail.com>
2024-02-03 11:36:23 +0100

    Create testing page

--- mysite/heroes_tracker/urls.py
+++ mysite/heroes_tracker/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:clan_name>/", views.testing_page, name="testing_page"),
 ]
--- mysite/heroes_tracker/views.py
+++ mysite/heroes_tracker/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "heroes_tracker/index.html")
+
+def testing_page(request, clan_name):
+    return render(request, "heroes_tracker/testing_page.html", {"clan_name": clan_name})
