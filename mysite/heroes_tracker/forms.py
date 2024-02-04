f8a92eb907e0ebbe9274451a2e3bc22b34343d34
Neeyo <jakub.nycz99@gmail.com>
2024-02-04 15:26:24 +0100

    Create forms for db models

---
+++ mysite/heroes_tracker/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import MapGroup, Map, Hero
+from django.contrib.auth.models import User
+
+class MapGroupForm(forms.ModelForm):
+    class Meta:
+        model = MapGroup
+        fields = ['name', 'description']
+
+class MapForm(forms.ModelForm):
+    class Meta:
+        model = Map
+        fields = ['name', 'description', 'updated_at', 'updated_by', 'map_group']
+
+class HeroForm(forms.ModelForm):
+    class Meta:
+        model = Hero
+        fields = ['name', 'description', 'lvl', 'maps']
